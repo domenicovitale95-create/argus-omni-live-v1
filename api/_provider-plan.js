9bbb8ed95e78f695d268086efd536c3b5b77f9cc
domenicovitale95-create <domenicovitale95@gmail.com>
2026-08-22 13:36:14 +0200

    config: register paid API-Football 7500 daily plan

---
+++ api/_provider-plan.js
@@ -0,0 +1,2 @@
+export const API_FOOTBALL_PLAN={provider:'API-FOOTBALL',subscription:'PAID',dailyLimit:7500,source:'OWNER_CONFIRMED',confirmedAt:'2026-08-22'};
+export function providerPlanMeta(){return{...API_FOOTBALL_PLAN}}
