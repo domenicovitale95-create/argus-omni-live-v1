3b8c51bd9f56700816714a3c0b3b400fe8000ea4
domenicovitale95-create <domenicovitale95@gmail.com>
2026-08-24 16:00:10 +0200

    fix: preserve cron headers in training capture wrapper

--- api/training-bets-cron.js
+++ api/training-bets-cron.js
@@ -1,6 +1,11 @@
 import trainingBets from './training-bets-v1.js';
 
 export default async function handler(req,res){
-  const nextReq={...req,method:'GET',query:{...(req.query||{}),mode:'run'}};
+  const nextReq={
+    ...req,
+    method:'GET',
+    headers:req.headers||{},
+    query:{...(req.query||{}),mode:'run'}
+  };
   return trainingBets(nextReq,res);
 }
