2730285046dabdef9b7545e2bd656ca61644b783
domenicovitale95-create <domenicovitale95@gmail.com>
2026-09-07 01:27:10 +0200

    Cache simplified LUCE core journeys

--- luce/sw.js
+++ luce/sw.js
@@ -1,5 +1,5 @@
-const CACHE='luce-v5';
-const CORE=['/luce/','/luce/book.html','/luce/book-data.js','/luce/film.html','/luce/journal.html','/luce/threshold.html','/luce/revelation.html','/luce/icon.svg','/luce/manifest.webmanifest'];
+const CACHE='luce-v6';
+const CORE=['/luce/','/luce/book.html','/luce/book-data.js','/luce/film.html','/luce/journal.html','/luce/threshold.html','/luce/revelation.html','/luce/chapter51.html','/luce/experience.html','/luce/icon.svg','/luce/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
