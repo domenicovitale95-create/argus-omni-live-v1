0236472c2cd503b7fe9ead425b00673968e8b4a5
domenicovitale95-create <domenicovitale95@gmail.com>
2026-09-07 01:26:11 +0200

    fix(immo): restore valid listing scanner parser

--- api/immo-listing-scan.js
+++ api/immo-listing-scan.js
@@ -12,12 +12,7 @@ function cleanText(v=''){
     .replace(/&#39;/g,"'").replace(/&quot;/g,'"').replace(/\s+/g,' ').trim();
 }
 function meta(html,key){
-  const escaped=key.replace(/[.*+?^$()|[\]\\]/g,'\\function meta(html,key){
-  const escaped=key.replace(/[.*+?^$()|[\]\\]/g,'\\$&');
-  const re1=new RegExp('<meta[^>]+(?:property|name)=["\\']'+escaped+'["\\'][^>]+content=["\\']([^"\\']*)["\\'][^>]*>','i');
-  const re2=new RegExp('<meta[^>]+content=["\\']([^"\\']*)["\\'][^>]+(?:property|name)=["\\']'+escaped+'["\\'][^>]*>','i');
-  return cleanText((html.match(re1)||html.match(re2)||[])[1]||'');
-}');
+  const escaped=String(key).replace(/[.*+?^$()|[\]\\]/g,'\\$&');
   const re1=new RegExp(`<meta[^>]+(?:property|name)=["']${escaped}["'][^>]+content=["']([^"']*)["'][^>]*>`,'i');
   const re2=new RegExp(`<meta[^>]+content=["']([^"']*)["'][^>]+(?:property|name)=["']${escaped}["'][^>]*>`,'i');
   return cleanText((html.match(re1)||html.match(re2)||[])[1]||'');
