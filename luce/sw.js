d0b075635f0ce2984d4e7359f5641c959cbaad2c
domenicovitale95-create <domenicovitale95@gmail.com>
2026-09-07 01:07:22 +0200

    Cache LUCE final threshold experience

--- luce/sw.js
+++ luce/sw.js
@@ -1,5 +1,5 @@
-const CACHE='luce-v3';
-const CORE=['/luce/','/luce/book.html','/luce/book-data.js','/luce/film.html','/luce/journal.html','/luce/icon.svg','/luce/manifest.webmanifest'];
+const CACHE='luce-v4';
+const CORE=['/luce/','/luce/book.html','/luce/book-data.js','/luce/film.html','/luce/journal.html','/luce/threshold.html','/luce/icon.svg','/luce/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
