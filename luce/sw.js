05cb5107390477b744ddeb5fcbd0937f8b6ea3ff
domenicovitale95-create <domenicovitale95@gmail.com>
2026-09-07 00:49:55 +0200

    Refresh LUCE offline cache version

--- luce/sw.js
+++ luce/sw.js
@@ -1,4 +1,4 @@
-const CACHE='luce-v1';
+const CACHE='luce-v3';
 const CORE=['/luce/','/luce/book.html','/luce/book-data.js','/luce/film.html','/luce/journal.html','/luce/icon.svg','/luce/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
